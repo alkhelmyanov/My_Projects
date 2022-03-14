dc3d27ab02eedf87995dfbe1659ad519863d1242
Khelmyanov_Alexander <alkhelmyanov@gmail.com>
2022-03-14 14:54:25 +0300

    Create class Calculator

--- src/main/java/CourierDeliveryCalculator/Calculator.java
+++ src/main/java/CourierDeliveryCalculator/Calculator.java
@@ -1,4 +1,13 @@
 package CourierDeliveryCalculator;
 
 public class Calculator {
+    public static void main(String[] args) {
+        int day; // кол-во отработанных дней
+        int workTime; // кол-во отработанного времени
+        int fuel; // топливо
+        int oil; // масло
+        int mileage; // пробег
+        int orders; // заказы
+
+    }
 }
